e10833aad71682d56fb35915a92456861c449c9f
hussein <hseinhalloum00@gmail.com>
2020-10-21 00:05:34 +0300

    documented list,add,remove

--- tasks.js
+++ tasks.js
@@ -100,7 +100,7 @@ function quit() {
 * @returns {void}
 */
 function help() {
-  console.log('1-hello, you can add your name too and it will shows "hello urname!"\n2-quit or exit\n3-help')
+  console.log('1- hello, you can add your name too and it will shows "hello urname!"\n2- quit or exit\n3- help\n4- list\n5- add\n6- remove')
 }
 
 function listTasks(){
